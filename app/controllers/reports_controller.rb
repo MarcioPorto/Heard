d3f6decd4784f0e01ef50a8ec0695b1971a53a6f
MarcioPorto <mflporto@gmail.com>
2016-04-09 19:45:15 -0500

    removed required report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -91,7 +91,7 @@ class ReportsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def report_params
-    params.require(:report).permit(
+    params.permit(
       :address,
       :latitude,
       :longitude,
